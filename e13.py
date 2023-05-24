2226db1a06c6eeef3289c6114860a7e7c4448bcb
josemoncada87 <josemoncada87@hotmail.com>
2023-05-24 01:04:10 -0500

    sdfsdf

--- e13.py
+++ e13.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 button_pin = 20
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin, GPIO.IN)
 
 # Estado inicial del botón (asumiendo que está en posición OFF)
 button_state = GPIO.input(button_pin)
