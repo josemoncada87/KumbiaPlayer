9e46275ce06b52ecc24e61969d8a2f1f052ea850
josemoncada87 <josemoncada87@hotmail.com>
2023-05-24 00:29:30 -0500

    sad

--- e12.py
+++ e12.py
@@ -66,8 +66,8 @@ def handle_potentiometer_change(potentiometer_value):
     global volume1, volume2, volume3, volume4
     global general_volume
 
-    general_volume = potentiometer_value
-   
+    general_volume = potentiometer_value / 100.0
+
     mixer1.set_volume(volume1 * general_volume)
     mixer2.set_volume(volume2 * general_volume)
     mixer3.set_volume(volume3 * general_volume)
